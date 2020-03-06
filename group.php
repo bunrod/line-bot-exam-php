f6c0cd85002e0f38a009e4dc743309599f04d032
bunrod <61861394+bunrod@users.noreply.github.com>
2020-03-06 13:17:08 +0700

    Update group.php

--- group.php
+++ group.php
@@ -1,5 +1,6 @@
 <php 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('ynV7OSbgW8UlzOBx9qhiq36u2kr3yYLDA4X8L+wMUTATsdMOYLVJ7wdS5yzfkpP5Li4yrZ9rY5lafnA6f+yucaBilbI6O834tnIiXapVybsVEDvqDBbJhQLqmrrRdm9dyNGn1Tyh75a6BWefA+limwdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'https://car-care-banknook.herokuapp.com/webhooks.php']);
 $response = $bot->getGroupMemberIds(<groupId>, <continuationToken>);
+  echo $response;
 ?>
