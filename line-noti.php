a62a37e703f7e9b0203ba9ba1153f56892dfadc5
bunrod <61861394+bunrod@users.noreply.github.com>
2020-05-11 13:24:24 +0700

    Update line-noti.php

--- line-noti.php
+++ line-noti.php
@@ -8,6 +8,7 @@ $userid = $_GET['userid'];
  $name_pk = $_GET['name_pk'];
  $license = $_GET['license'];
 
+echo 'TEST';
 $access_token = 'ynV7OSbgW8UlzOBx9qhiq36u2kr3yYLDA4X8L+wMUTATsdMOYLVJ7wdS5yzfkpP5Li4yrZ9rY5lafnA6f+yucaBilbI6O834tnIiXapVybsVEDvqDBbJhQLqmrrRdm9dyNGn1Tyh75a6BWefA+limwdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '5e48ec085f1d1c42c86b9cf8486abbeb';
 $idPush = 'U9126c7bdf7e1a36a8d18919dc600429d';
@@ -17,9 +18,9 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('มีรายการจองใหม่ ชื่อ : '.$name.' ทะเบียนรถ  : '.$license.' บริการที่เลือก  : '.$name_pk.' วัน/เวลาที่จะใช้บริการ '.$date.' '.$time.'  น.');
 // $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('มีรายการจองใหม่ ชื่อ ');
 $response = $bot->pushMessage($idPush, $textMessageBuilder);
+
 // $response = $bot->pushMessage($idPush,(new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder())->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('text1', 'text2'));
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-echo 'TEST';
 // if($count >= 10 )
 // {
 //     $access_token = 'ynV7OSbgW8UlzOBx9qhiq36u2kr3yYLDA4X8L+wMUTATsdMOYLVJ7wdS5yzfkpP5Li4yrZ9rY5lafnA6f+yucaBilbI6O834tnIiXapVybsVEDvqDBbJhQLqmrrRdm9dyNGn1Tyh75a6BWefA+limwdB04t89/1O/w1cDnyilFU=';
